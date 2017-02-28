b4b9d7a90f568eaff4b9380549267a80d125363c
Schuyler Smith <schuyler@jareds-air2.student.iastate.edu>
2017-02-28 13:02:32 -0600

    Moved stuff around because I'm unorganized with files

--- Entrez_Query_NCBI/Run_UOs.sh
+++ Entrez_Query_NCBI/Run_UOs.sh
@@ -8,6 +8,6 @@ Output=${@:$#}
 
 cat $Input > $the_path/tmp_file.txt
 
-python $the_path/Unique_Orgs.py $the_path/tmp_file.txt $Output
+python $the_path/Working_Files/Unique_Orgs.py $the_path/tmp_file.txt $Output
 
 rm $the_path/tmp_file.txt
